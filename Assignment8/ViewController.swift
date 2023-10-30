702253307d12e171cf0e63537bde32096175a0ec
Suguru Tokuda <suguru.tokuda@gmail.com>
2023-10-30 15:30:17 -0400

    Initial Commit

---
+++ Assignment8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment8
+//
+//  Created by Suguru Tokuda on 10/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
